28d1af21c8977bf96dd6eefced4e9990852f234e
Greg Wilson <greg@gbwilson.com>
2022-02-06 15:39:26 -0500

    GeoJSON.js:221 Uncaught Error: Invalid GeoJSON object.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -10,7 +10,9 @@ console.log("working");
 // Create the map object with center at the San Francisco airport. For Lines
 // let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 // Create the map object with center at the San Francisco airport. For GeoJSON
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level. For GeoJSON in repo
+let map = L.map('mapid').setView([30, 30], 3);
 
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -32,8 +34,8 @@ let map = L.map('mapid').setView([37.5, -122.5], 10);
 //     fillColor: '#ffffa1'
 //     }).addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// // Get data from cities.js
+// let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 // cityData.forEach(function(city) {
@@ -42,31 +44,31 @@ let cityData = cities;
 //   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 // .addTo(map);
 // });
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {radius:city.population/100000})
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-});
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {radius:city.population/100000})
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+// .addTo(map);
+// });
 
-// Coordinates for each point to be used in the line.
-// Coordinates for each point to be used in the polyline.
-let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790],
-  [40.7899, -111.9791],
-  [47.4502, -122.3088]
-];
+// // Coordinates for each point to be used in the line.
+// // Coordinates for each point to be used in the polyline.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790],
+//   [40.7899, -111.9791],
+//   [47.4502, -122.3088]
+// ];
 
 // // Create a polyline using the line coordinates and make the line red.
 // L.polyline(line, {
 //   color: "red"
 // }).addTo(map);
 
-// Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-  color: "yellow"
-}).addTo(map);
+// // Create a polyline using the line coordinates and make the line yellow.
+// L.polyline(line, {
+//   color: "yellow"
+// }).addTo(map);
 
 
 
@@ -91,32 +93,32 @@ L.polyline(line, {
 // id = 'mapbox/satellite-v9'
 // id = 'mapbox/satellite-streets-v11'
 ///////////////////////// GeoJSON Layer Start //////////////////////////////////////////
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-// Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  onEachFeature: function(_feature, layer) {
-    console.log(layer);
-    layer.bindPopup();
-}
-}).addTo(map);
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   onEachFeature: function(_feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup();
+// }
+// }).addTo(map);
 
 // // Grabbing our GeoJSON data.
 // L.geoJSON(sanFranAirport, {
@@ -131,7 +133,7 @@ L.geoJSON(sanFranAirport, {
 
 ///////////////////////// GeoJSON Layer End //////////////////////////////////////////
 
-
+///////////////////////// Title Layer Method Start ////////////////////////////////////
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 // We create the tile layer that will be the background of our map.
@@ -141,7 +143,33 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-
-
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+///////////////////////// Title Layer Method End ////////////////////////////////////
+
+
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/GregoryBWilson/Mapping_Earthquakes/main/majorAirports.json";
+
+//////////////////////// d3.json() Method Start ///////////////////////////////////
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
+//////////////////////// d3.json() Method End ///////////////////////////////////
+// GeoJSON.js:221 Uncaught Error: Invalid GeoJSON object. see line 169//
+
+// Grabbing our GeoJSON data.
+L.geoJSON(airportData, {
+  onEachFeature: function(_feature, layer) {
+    // console.log(`_feature = `,_feature);
+    // console.log(`layer = `,layer);
+    layer.bindPopup();
+}
+}).addTo(map);
