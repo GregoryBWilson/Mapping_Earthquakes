9c721e1c93c66ebedf20066d3f1e210403dbab6f
Greg Wilson <greg@gbwilson.com>
2022-02-07 14:21:00 -0500

    Multiple Maps added

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -12,7 +12,7 @@ console.log("working");
 // Create the map object with center at the San Francisco airport. For GeoJSON
 // let map = L.map('mapid').setView([37.5, -122.5], 10);
 // Create the map object with center and zoom level. For GeoJSON in repo
-let map = L.map('mapid').setView([30, 30], 3);
+// let map = L.map('mapid').setView([30, 30], 3);
 
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -143,8 +143,33 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
 
 ///////////////////////// Title Layer Method End ////////////////////////////////////
 
@@ -185,3 +210,4 @@ d3.json(airportData).then(function(data) {L.geoJSON(data, {
 }).addTo(map);
 })
 //////////////////////// d3.json() Method End ///////////////////////////////////
+
